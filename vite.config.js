358620045c4ae972147fc8f0b770140c98f32268
Shonky94 <shounakkhadilkar@gmail.com>
2024-11-30 02:58:05 +0530

    version

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      external: ['react-router-dom'], // Add other problematic modules here if needed
-    },
-  },
+    outDir: 'dist',  // Ensure the output directory is 'dist'
+  }
 });
